66968bf3931ce2b10b02da5398be52ef924426a3
Oleksandr Bondarenko <oleksandr_bondarenko2@epam.com>
2021-05-12 12:50:11 +0300

    build: ci/cd
    - upd nuxt Doker image build configs

---
+++ admin/ecosystem.config.js
@@ -0,0 +1,14 @@
+const { join } = require('path')
+
+module.exports = {
+  apps: [
+    {
+      name: 'AdminApp',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: `-c ${join(__dirname, 'nuxt.config.js')}`,
+      cwd: './'
+    }
+  ]
+}
